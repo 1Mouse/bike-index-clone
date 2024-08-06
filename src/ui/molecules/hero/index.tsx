c0566e17fb05d2b7b853b5d8c8c1a1deffc78e33
1Mouse <melfar391@gmail.com>
2024-08-06 11:06:49 +0300

    add hero molecule

---
+++ src/ui/molecules/hero/index.tsx
@@ -0,0 +1,15 @@
+
+
+export function Hero() {
+  return (
+    <>
+      <header className='space-y-4 p-6 text-center'>
+        <h1 className='text-4xl font-bold'>Bike Index</h1>
+        <p className='text-xl'>
+          Welcome back officer👋, Let&apos;s automatically keep track of
+          <span className='font-trade-winds'> bikes.</span>
+        </p>
+      </header>
+    </>
+  );
+}
