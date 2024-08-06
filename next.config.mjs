2b2d738fd5f3d7597529a646f7f42be25afb3a65
1Mouse <melfar391@gmail.com>
2024-08-06 10:47:18 +0300

    clean up tempalte and add assets

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "bikebook.s3.amazonaws.com",
+      },
+      {
+        hostname: "files.bikeindex.org",
+      },
+      {
+        hostname: "localhost",
+      },
+      {
+        hostname: "127.0.0.1",
+      },
+    ],
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+};
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/ui/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        images: "url('./public/loader.svg')",
+      },
+      fontFamily: {
+        inter: ["var(--font-inter)", ...defaultTheme.fontFamily.sans],
+        "trade-winds": [
+          "var(--font-trade-winds)",
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+      colors: {
+        black: "#252523",
+        lime: "#D7F03A",
+        purple: "#533AF0",
+        white: "#F6F8FA",
+        cream: "#FFFFF0",
       },
     },
   },
-  plugins: [],
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
